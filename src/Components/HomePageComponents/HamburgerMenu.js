6f6c611795c8effd5557c72c44ed12e105a85eaa
Eric <shantz.eric@gmail.com>
2023-08-30 15:45:42 -0400

    create slider on mobile view

--- src/Components/HomePageComponents/HamburgerMenu.js
+++ src/Components/HomePageComponents/HamburgerMenu.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const HamburgerMenu = () => {
+    const handleClick = () =>{
+        console.log('CLICKED')
+    }
   return (
     <div>
-      <div> Option 1</div>
-      <div> Option 2</div>
-      <div> Option 3</div>
+      <div onClick={handleClick}> Option 1</div>
+      <div onClick={handleClick}> Option 2</div>
+      <div onClick={handleClick}> Option 3</div>
     </div>
   )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Searchbar from "../Components/HomePageComponents/Searchbar";
 import Sidebar from "../Components/HomePageComponents/Sidebar";
 import Dashboard from "../Components/HomePageComponents/MainContent/Dashboard"
@@ -6,60 +6,119 @@ import Interviews from "../Components/HomePageComponents/MainContent/Interviews"
 import Settings from "../Components/HomePageComponents/MainContent/Settings"
 import HamburgerMenu from "../Components/HomePageComponents/HamburgerMenu"
 import MenuIcon from '@mui/icons-material/Menu';
+import 'animate.css';
 import "../Styles/HomePageComponents/home-page.css"
 
+// const Home = () => {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setWindowWidth(window.innerWidth);
+//     };
+
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     }
+//   }, []);
+
+//   const toggleSidebar = (e) => {
+//     const isHamburgerMenuClicked = e.target.closest('.mobile-hamburger-menu');
+//     if (isHamburgerMenuClicked) {
+//       return; // Prevent toggling if the target is the hamburger menu
+//     }
+//     if(sidebarOpen){
+
+//     }
+//     setSidebarOpen(!sidebarOpen);
+
+//   };
+
+//   return (
+//     <div className="home-page">
+//       {windowWidth >= 600 && (
+//         <div className="sidebar">
+//           <Sidebar />
+//         </div>
+//       )}
+
+//       <div className={`searchbar-mainContent-stack ${sidebarOpen ? 'sidebar-open' : ''}`}>
+//         <div className="searchbar-hamburger-stack">
+//           {windowWidth < 600 && (
+//             <MenuIcon className="menu-icon" onClick={toggleSidebar}/>
+//           )}
+//           <div className="searchbar">
+//             <Searchbar />
+//           </div>
+//         </div>
+
+//         {/* Main Content */}
+//         <Dashboard />
+//       </div>
+
+// {/*============== Hamburger Menu - Mobile =============*/}
+//       {windowWidth < 600 && (
+//         <div className={`hamburger-overlay`} onClick={toggleSidebar}>
+//           <div
+//             className={`mobile-hamburger-menu animate__animated ${
+//               sidebarOpen
+//                 ? 'animate__slideInLeft'
+//                 : 'animate__slideOutLeft'
+//             } animate__faster`}
+//           >
+//             <HamburgerMenu />
+//           </div>
+//         </div>
+//       )}
+
+//     </div>
+//   );
+// }
+
+// export default Home;
+
 const Home = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false); // State to track sidebar
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      }
+    }, []);
 
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
+    const toggleSideBar = () => {
+      setSidebarOpen(!sidebarOpen)
     }
-  })
-
-  const toggleSidebar = () => {
-    console.log("TOGGLING")
-    setSidebarOpen(!sidebarOpen);
-  };
-
+  
   return (
-    <div className="home-page">
-      {windowWidth >= 600 ? (
-        <div className="sidebar">
-          <Sidebar />
-        </div>
-      ) : (
-        <div className="hamburger-menu">
+    <div>
+    {windowWidth <= 600 ? (
+      <div>
+        <div className={` ${sidebarOpen ? "overlay" : ''}` } onClick={toggleSideBar}/>
+        <div className={`sidebar ${!sidebarOpen ? "sidebar-closed" : "sidebar-open"}`}>
           <HamburgerMenu />
         </div>
-      )}
-
-      <div className={`searchbar-mainContent-stack`}>
-        <div className="searchbar-hamburger-stack">
-          {windowWidth < 600 && (
-            <MenuIcon className="menu-icon" onClick={toggleSidebar}/>
-          )}
-          <div className="searchbar">
-            <Searchbar />
-          </div>
+        <div className="mobile-header">
+          <MenuIcon className="menu-icon" onClick={toggleSideBar} />
+          <Searchbar />
         </div>
-
-        {/* Main Content */}
-        <Dashboard />
       </div>
-    </div>
-  );
+    ) : (
+      <div>{/* Desktop View */}</div>
+    )}
+  </div>
+  
+  )
 }
-
-
 export default Home;
 
-
-   
